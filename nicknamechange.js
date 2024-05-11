9c652bd56d7d89f75b0a77ecf6618383c3f11937
dttric <87319664+dttric@users.noreply.github.com>
2024-05-12 02:02:37 +0700

    для начала сойдет

---
+++ nicknamechange.js
@@ -0,0 +1,11 @@
+var nameIndex = 0;
+var nicknames = ["dttric", "Сайлент", "Лягушка", "toadtg"];
+
+function changeNameAutomatically() {
+    $("#nicknamechange").fadeOut(500, function() {
+        $(this).text(nicknames[nameIndex]).fadeIn(500);
+    });
+    nameIndex = (nameIndex + 1) % nicknames.length;
+}
+
+setInterval(changeNameAutomatically, 1700);
